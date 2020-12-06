4b75b62ac01663e9e04ef53527ef7ba811ccd304
Brian Leyva <55462869+xxSShadow@users.noreply.github.com>
2020-12-05 20:46:02 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
-// Import stylesheets
-import './style.css';
+Discord = require('discord.js');
+Bot = new Discord.Client();
+Prefix = ("Enter Prefix Here");
+Token = ("Enter Token Here");
+Games = ["Enter Games Here"];
+Game = Games[Math.floor(Math.random()*Games.length)];
+Activity = ["PLAYING", "STREAMING", "WATCHING"];
+Currently = Activity[Math.floor(Math.random()*Activity.length)];
+Status = ("Online")
 
-// Write Javascript code!
-const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Starter</h1>`;
+if (Currently == "PLAYING") {Mode = ("Playing")}
+if (Currently == "STREAMING") {Mode = ("Streaming")}
+if (Currently == "WATCHING") {Mode = ("Watching")}
+
+Bot.once('ready', () => {
+  console.info(`Name: ${Bot.user.username}`);
+  console.info(`Bot Status: ${Status}!`);
+  console.info(`Currently ${Mode}: ${Game}™!`);
+  Bot.user.setActivity(Game, {type: `${Currently}`})
+});
+
+Bot.login(Token)
